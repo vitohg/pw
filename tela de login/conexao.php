c31a24a148dfcaa5a73fe6e0ed3ac18340dfa8eb
vitohg <victorhugo036510@gmail.com>
2022-10-12 19:23:14 -0300

    commit

---
+++ tela de login/conexao.php
@@ -0,0 +1,11 @@
+<?php       
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'vitohugo_login');
+
+$connection = mysqli_connect(DB_SERVER, DB_USERNAME , DB_PASSWORD, DB_DATABASE);
+if ($connection === false) {
+    die("ERROR: Não pode conectar ao banco!" . mysqli_connect_error());
+} 
+?>  
